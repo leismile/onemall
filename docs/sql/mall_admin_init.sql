a17cbd380a28b2834be78e1a3c88614763a49aa0
lei.sunlsl <lei.sunlsl@alibaba-inc.com>
2019-09-05 15:57:54 +0800

    add admin init sql.

---
+++ docs/sql/mall_admin_init.sql
@@ -0,0 +1,66 @@
+INSERT INTO `mall_admin`.`admin` (`id`, `username`, `nickname`, `password`, `status`, `deptment_id`, `create_time`,`deleted`) 
+VALUES ('1', 'admin', 'admin', '21232f297a57a5a743894a0e4a801fc3', '0', '0', now(), b'0');
+
+
+INSERT INTO `mall_admin`.`role` (`id`, `name`, `create_time`,`deleted`) VALUES ('1', '超级管理员', now(), b'0');
+
+
+INSERT INTO `mall_admin`.`admin_role` (`id`, `admin_id`, `role_id`,`create_time`,`deleted`) VALUES ('1', '1', '1',now(), b'0');
+
+
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('1', '1', '1', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('2', '1', '2', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('3', '1', '3', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('4', '1', '8', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('5', '1', '9', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('6', '1', '10', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('7', '1', '11', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('8', '1', '12', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('9', '1', '13', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('10', '1', '14', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('11', '1', '15', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('12', '1', '16', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('13', '1', '17', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('14', '1', '18', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('15', '1', '19', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('16', '1', '20', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('17', '1', '21', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('18', '1', '22', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('19', '1', '23', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('20', '1', '24', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('21', '1', '25', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('22', '1', '26', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('23', '1', '27', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('24', '1', '28', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('25', '1', '29', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('26', '1', '30', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('27', '1', '31', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('28', '1', '32', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('29', '1', '33', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('30', '1', '34', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('31', '1', '35', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('32', '1', '36', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('33', '1', '37', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('34', '1', '38', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('35', '1', '39', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('36', '1', '40', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('37', '1', '41', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('38', '1', '42', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('39', '1', '43', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('40', '1', '44', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('41', '1', '45', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('42', '1', '46', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('43', '1', '47', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('44', '1', '48', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('45', '1', '49', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('46', '1', '50', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('47', '1', '51', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('48', '1', '52', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('49', '1', '53', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('50', '1', '54', now(), b'0');
+INSERT INTO `mall_admin`.`role_resource` (`id`, `role_id`, `resource_id`, `create_time`,`deleted`) VALUES ('51', '1', '55', now(), b'0');
+
+
+
+
+
