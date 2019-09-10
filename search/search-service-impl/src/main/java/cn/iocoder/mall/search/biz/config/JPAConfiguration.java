6694ec93df12f2da5e531b485e9d4c9f11664672
lei.sunlsl <lei.sunlsl@alibaba-inc.com>
2019-09-10 17:52:34 +0800

    modify for elasticsearch x-pack auth.

--- search/search-service-impl/src/main/java/cn/iocoder/mall/search/biz/config/JPAConfiguration.java
+++ search/search-service-impl/src/main/java/cn/iocoder/mall/search/biz/config/JPAConfiguration.java
@@ -6,4 +6,15 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "cn.iocoder.mall.search.biz.dao")
 public class JPAConfiguration {
+
+	@Bean
+    public TransportClient transportClient() throws UnknownHostException {
+        TransportClient client = new PreBuiltXPackTransportClient(Settings.builder()
+                .put("cluster.name", "docker-cluster")
+                .put("xpack.security.user", "elastic:MagicWord")
+                .build())
+                .addTransportAddress(new TransportAddress(InetAddress.getByName("127.0.0.1"), 9300));
+        return client;
+    }
+    	
 }
